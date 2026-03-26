7f4da8205672b1a8496f11816cd269a3773f3541
RMAC <rmac@stibosystems.com>
2026-03-26 16:32:21 -0400

    Committed from STEP 2026-03-26T20:32Z

---
+++ uspresales-americas/2026-03-27_Release_to_PROD/Primary Items/BusinessRule/BusinessRule_ApproveAction.js
@@ -0,0 +1,38 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Main"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "ApproveAction",
+  "type" : "BusinessAction",
+  "setupGroups" : [ "Actions" ],
+  "name" : "Approve Action",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ "ContactPerson", "OrganizationCustomer", "Supplier" ],
+  "allObjectTypesValid" : false,
+  "runPrivileged" : true,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "CurrentObjectBindContract",
+    "alias" : "node",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (node) {
+node.approve();
+}
